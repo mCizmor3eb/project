e59ad40d08c667ffd7a14e810a528e5c64cf3d5f
kikic <kikic@Ahmeds-MacBook-Air-2.local>
2021-07-31 10:06:20 +0100

    travis

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=kikic
+export POSTGRES_PASSWORD=a7med123
+export POSTGRES_HOST=cdr2.cztskm8s0ipi.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-129309386521-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
